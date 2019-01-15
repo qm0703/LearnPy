f91b4482efa425f336d0f36452bae8ce514fac3e
wkang <2272728555@qq.com>
2019-01-15 18:08:45 +0800

    Initial commit

---
+++ ch1_3.py
@@ -0,0 +1,26 @@
+print(2 + 2)
+# 浮点数相除
+print("浮点数相除:")
+print(1 / 2)
+print(1 / 1)
+# 整除
+print("整除:")
+print(1 // 2)
+print(1 // 1)
+print(5.0 // 2.4)
+# 求模
+print("求模:")
+print(1 % 2)
+print(2.75 % 0.5)
+# 整除向下圆整
+print("整除向下圆整:")
+print(10 // 3)
+print(10 // -3)
+print(-10 // 3)
+print(-10 // -3)
+# 乘方
+print("乘方:")
+# 乘方的优先级比单目减高
+print(2 ** 3)
+print(-3 ** 2)
+print((-3) ** 2)
---
+++ ch1_6.py
@@ -0,0 +1,4 @@
+# 获取用户输入
+x = input("x: ")
+y = input("y: ")
+print(int(x) * int(y))
