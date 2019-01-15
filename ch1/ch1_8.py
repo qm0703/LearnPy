6c3d106a64ff2756688564a7c8f854388a6bb676
jk9559 <2272728555@qq.com>
2019-01-16 01:07:18 +0800

    py

---
+++ ch1/ch1_8.py
@@ -0,0 +1,12 @@
+# 模块
+from math import sqrt
+import math
+
+# floor 函数 返回小于等于给定数的最大整数
+print(math.floor(32.9))
+print(math.floor(32))
+# ceil 函数 返回大于等于给定数的最小整数
+print(math.ceil(32.1))
+print(math.ceil(32))
+# 通过使用命令import的变种from module import function，可以在调用函数时，不指定模块前缀
+print(sqrt(9.0))
